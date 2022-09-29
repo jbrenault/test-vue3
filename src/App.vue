b8882481764a36bc875e2604d6242dc33f6d52d1
Jean Baptiste Renault <renault.jeanbaptiste@gmail.com>
2022-09-29 16:23:13 +0200

    chore: add lib front common

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,29 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <pc-configurator-header configurator-name="gate and fence"></pc-configurator-header>
+  <pc-selectable-model
+      id="dumb"
+      label="dumb"
+      img-src="https://www.francetvinfo.fr/pictures/si9iFOPV1UO8tFtNS10SPe3-MQM/1500x843/2016/08/23/internet-troll.jpg"
+      img-alt="gate default"
+      miscellaneous="à partir de"
+      pricing="1549.00€"
+  >
+    <div slot="title">Babord - Coulissant aspect 2 vantaux</div>
+    <div slot="subtitle">Aluminium · Semi-ajouré</div>
+    <div slot="content">
+      Hauteur : min 751 - max 3052 mm <br />
+      Largeur : min 751 - max 3052 mm
+    </div>
+  </pc-selectable-model>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import 'configurators-web-components/dist/pc.min.js'
 
 createApp(App).mount('#app')
